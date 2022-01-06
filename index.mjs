df3171f40ae7de2b3f185c2f0a7fa80f03cca878
ravindra.gatkawar <rg.gatkawar@gmail.com>
2022-01-06 13:33:44 +0530

    added index.mjs, ssl/
    index.mjs is entry point to app, starts the servers in app.mjs
    created ssl/ directory, it contains server key and certificate

---
+++ index.mjs
@@ -0,0 +1,24 @@
+import app from 'lib/app.mjs';
+import config from 'config.mjs';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname,join } from 'path/posix';
+
+
+const envName   = config.envName.toUpperCase();
+const httpPort  = config.httpPort;
+const httpsPort = config.httpsPort;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));  // get current working directory 
+console.log(`App Started In ${envName}`);
+
+
+//----- start http, https server -----
+
+app.http(httpPort);
+
+const httpsOptions ={
+    'key' : readFileSync(join(__dirname,'ssl/server.key')),
+    'cert': readFileSync(join(__dirname,'ssl/server.crt'))
+}
+app.https(httpsOptions,httpsPort);
