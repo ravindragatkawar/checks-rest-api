bf8cffaf915e195669f38771cc4b29c43cf9e1fd
ravindra.gatkawar <rg.gatkawar@gmail.com>
2022-01-06 12:28:44 +0530

    added configuration file: config.mjs
    config.mjs exports configuration/environment settings as object required by app

---
+++ config.mjs
@@ -0,0 +1,35 @@
+//----- environment for app -----
+//----- default environment is developement -----
+
+
+let envs = {};
+
+envs.developement =  {
+    'envName' : 'developement',
+    'httpPort': 3000,
+    'httpsPort' : 3001,
+    'secrete' : 'this is a secret for password enc'
+}
+
+envs.production = {
+    'envName' : 'production',
+    'httpPort': 80,
+    'httpsPort' : 443,
+    'secrete' : 'this is a secret for password enc'
+}   
+
+
+const cmdEnv = process.env.NODE_ENV;
+let config = {};
+
+//----- if NODE_ENV is not given on command line or not found in config environment(envs) -----
+//----- then set development as default app environment -----
+
+if (typeof cmdEnv == 'undefined' || typeof envs[cmdEnv] == 'undefined') {
+    config = envs.developement;
+}
+else {
+    config = envsp[cmdEnv];
+}
+
+export default config;
